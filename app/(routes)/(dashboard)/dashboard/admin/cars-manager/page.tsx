4798faa4b2ff1d416524aa9b138e21a0a7c997bd
sergio md <sergiomd@MacBook-Pro-de-sergio.local>
2024-09-02 17:01:56 +0200

    add button add-car

--- app/(routes)/(dashboard)/dashboard/admin/cars-manager/page.tsx
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/page.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
+import { ButtonAddCar } from './components/button-add-car';
 
-export default function CarsManagerPage() {
-  return <div>CarsManagerPage</div>;
+export default async function CarsManagerPage() {
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">organiza tus coches</h2>
+        <ButtonAddCar />
+      </div>
+    </div>
+  );
 }
