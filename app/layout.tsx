9e4d25c0e611eb85d71b72c55fe03f5781e29ac3
sergio md <sergiomd@MacBook-Pro-de-sergio.local>
2024-09-02 17:12:42 +0200

    Installing different packages for the form

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,9 @@ import type { Metadata } from 'next';
 import { Outfit } from 'next/font/google';
 import './globals.css';
 import NextTopLoader from 'nextjs-toploader';
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';
+
+import { Toaster } from '@/components/ui/toaster';
 
 const outfit = Outfit({ subsets: ['latin'] });
 
@@ -29,6 +25,7 @@ export default function RootLayout({
           <NextTopLoader color="#000" />
           {children}
         </body>
+        <Toaster />
       </html>
     </ClerkProvider>
   );
