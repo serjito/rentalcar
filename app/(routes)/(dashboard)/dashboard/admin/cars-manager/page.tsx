26e5740a8cbdf8c8beb1dd19e7ec48b7d844c670
sergio md <sergiomd@MBP-de-sergio.home>
2024-09-03 14:10:09 +0200

    install form

--- app/(routes)/(dashboard)/dashboard/admin/cars-manager/page.tsx
+++ app/(routes)/(dashboard)/dashboard/admin/cars-manager/page.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { ButtonAddCar } from './button-add-car';
 
 export default function CarsManagerPage() {
-  return <div>CarsManagerPage</div>;
+  return (
+    <div>
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold">Manage your cars</h2>
+        <ButtonAddCar />
+      </div>
+    </div>
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,13 +2,8 @@ import type { Metadata } from 'next';
 import { Outfit } from 'next/font/google';
 import './globals.css';
 import NextTopLoader from 'nextjs-toploader';
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs';
+import { ClerkProvider } from '@clerk/nextjs';
+import { Toaster } from '@/components/ui/toaster';
 
 const outfit = Outfit({ subsets: ['latin'] });
 
@@ -28,6 +23,7 @@ export default function RootLayout({
         <body className={outfit.className}>
           <NextTopLoader color="#000" />
           {children}
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>
